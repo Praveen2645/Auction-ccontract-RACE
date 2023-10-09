c75ec9f080d80fd9680ea43300c599e4b64f870d
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-10-09 19:57:06 +0530

    Update race-Auction.sol

--- race-Auction.sol
+++ race-Auction.sol
@@ -14,55 +14,55 @@ contract RaceAuction is Ownable{
  error RaceAuction__FailedToTransferNFT();
  error RaceAuction__InvalidIpfsHash();
  
-
  struct Auction{
  uint256 tokenId;
  uint256 basePrice;
- uint256 duration;//duration of auction t
-// uint256 startAt;//auction start time
- //uint256 endAt; //auction endtime
- string  ipfsHash;
+ uint256 duration;//duration of auction
+uint256 startAt;//auction start time
+ uint256 endAt; //auction ending time
+ //string  ipfsHash;
  address payable[] bidders;
  address seller;
  bool ended;
  }
  
  address highestBidder;
- uint256 AuctionCount;
+ uint256 public AuctionCount;
  IERC721  NFT;
  uint256 highestBid;
  Auction auction;
  
- mapping (address admin => bool) admins;
- mapping(uint256 AuctionCount => Auction) auctionCountToAuction;
- mapping(address bidders => uint256 amount)  biddersToAmount;
+ mapping(uint256 tokenId=>Auction)public AuctionDetails;
+ mapping (address admin => bool) public admins;
+ //mapping(uint256 AuctionCount => Auction) public auctionCountToAuction;
+ mapping(address bidders => uint256 amount)  public biddersToAmount;
 
 ////////////////////////////
    ///FUNCTIONS///
 ////////////////////////////   
 
- function setNftContract(address _address) external onlyOwner returns (bool){
+ function setNftContract(address _address) external onlyOwner returns (bool){ //onlyDIC
  NFT = IERC721(_address);
  return true;
 
  }
 
- function addNFT(uint256 _tokenId,uint256 _basePrice, uint256 _duration, string memory _ipfsHash) external view returns (bool){ //investor
- _addNFT(_tokenId, _basePrice, _duration,_ipfsHash);
+ function addNFT(uint256 _tokenId,uint256 _basePrice, uint256 _duration/*, string memory _ipfsHash*/) external view returns (bool){//DIC
+ _addNFT(_tokenId, _basePrice, _duration/*,_ipfsHash*/);
  return true;
  }
 
- function addAdmin(address _address) external onlyOwner returns(bool){//onlyDIC
- admins[_address] = true;
- return true;
- }
+//  function addAdmin(address _address) external onlyOwner returns(bool){//adding admin by access control
+//  admins[_address] = true;
+//  return true;
+//  }
 
  function removeAdmin(address _address) external onlyOwner returns(bool){//onlyDIC
  admins[_address] =false;
  return true;
  }
 
- function _addNFT(uint256 _tokenId, uint256 _basePrice, uint256 _duration, string memory _ipfsHash) internal view returns (bool){
+ function _addNFT(uint256 _tokenId, uint256 _basePrice, uint256 _duration /*,string memory _ipfsHash*/) internal view returns (bool){//
  if(NFT.ownerOf(_tokenId) !=msg.sender){
  revert RaceAuction__NotAdmin();
  }
@@ -73,14 +73,11 @@ contract RaceAuction is Ownable{
     revert RaceAuction__InvalidDuration();
  }
 
- 
-//  
 return true;
-    }
-
+ }
  
 
- function CreateAuction(uint256 _tokenId, uint256 _basePrice, uint256 _duration) external onlyOwner() { //onlyDIC
+ function CreateAuction( uint256 _tokenId, uint256 _basePrice, uint256 _duration) external onlyOwner() { //onlyDIC
     if (NFT.ownerOf(_tokenId) != msg.sender) {
         revert RaceAuction__NotAdmin();
     }
@@ -91,17 +88,22 @@ return true;
         revert RaceAuction__InvalidDuration();
     }
 
-    //Auction storage auction = auctionCountToAuction[AuctionCount];
-    auction.tokenId = _tokenId;
-    auction.basePrice = _basePrice;
-    auction.duration = block.timestamp + _duration;
- //   auction.startBlock= block.timestamp;
-   // auction.endBlock = 
-    auction.ended = false;
+    AuctionDetails[_tokenId].tokenId = _tokenId;
+    AuctionDetails[_tokenId].duration = _duration;
+    AuctionDetails[_tokenId].basePrice = _basePrice;
+    AuctionDetails[_tokenId].seller = msg.sender;
+    AuctionDetails[_tokenId].startAt = block.timestamp;
+    AuctionDetails[_tokenId].endAt = _duration+ block.timestamp;
+
+    NFT.transferFrom(msg.sender,address(this),_tokenId);
+   require(NFT.ownerOf(_tokenId) == address(this),"failed to tranfer nft");
+   
     AuctionCount++;
 }
 
-function placeBid(uint256 _itemId) public payable {}
+function placeBid(uint256 _itemId) public payable {
+
+}
 
 function cancelAuction(uint256 _itemId) external {}
 
