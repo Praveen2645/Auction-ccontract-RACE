b6e029201421949d90ed73bcc5ca82f8c6177215
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-10-18 19:53:25 +0530

    Update race-Auction.sol

--- race-Auction.sol
+++ race-Auction.sol
@@ -17,21 +17,6 @@ import "contracts/Admin.sol";
 
 contract RaceAuction is AccessControl, ReentrancyGuard{
 
-     //////////////////
-       // errors //
-    ///////////////// 
-
- error RaceAuction__InvalidBasePrice();
- error RaceAuction__InvalidDuration();
- error RaceAuciton__NotNftOwner();
- error RaceAuction__BidAmountTooLow();
- error RaceAuction__AuctionFinished();
- error RaceAuction__NftTransferFailed();
- error RaceAuction__OngoingAuction();
- error RaceAuction__NotWinner();
- error RaceAuction__InsufficientBalance();
- //error RaceAuction__InvalidIpfsHash();
-
    ///////////////////////
      //State Variable //
   ////////////////////////
@@ -42,7 +27,6 @@ contract RaceAuction is AccessControl, ReentrancyGuard{
  address public admin;
  Admin public immutable ADMIN_CONTRACT_ADD;
 
-
  struct Auction{
  uint256 tokenId;
  uint256 basePrice;
@@ -52,7 +36,7 @@ contract RaceAuction is AccessControl, ReentrancyGuard{
  uint256 highestBid;
  address highestBidder; 
  address payable[] bidders;
- //string  ipfsHash;
+ 
  }
 
  mapping(uint256 tokenId => Auction)public AuctionDetails;
@@ -86,7 +70,10 @@ contract RaceAuction is AccessControl, ReentrancyGuard{
         ADMIN_CONTRACT_ADD = adminContractAddress;
     }
 
-
+  /* 
+  *@param _NftContractAddress - address of the NFT contract
+  *@notice This function sets the NFT Contract address for this Auction contract 
+  */
  function setNftContract(address _NftContractAddress) external onlyAdmin returns (bool) { 
  NFT = IERC721(_NftContractAddress);
  return true;
@@ -116,14 +103,19 @@ contract RaceAuction is AccessControl, ReentrancyGuard{
     emit AuctionInitialized (_tokenId, _basePrice, _duration, msg.sender);
 
     require(NFT.ownerOf(_tokenId) == address(this),"NFT transfer failed");
-
     AuctionCount++;
 }
-
+ /* 
+  *@param _tokenId- tokenId of the NFT
+  *@notice This function place bid for the NFTs whic are out for Auction.
+  *This function have some checks, for checking the Auction is live or not, 
+  *checks the bid amount,which should be high than the previous bid amount.
+  *Also the previous highest bidder will get his bidded amount back as the new bidder bid the high amount
+  */
 
 function PlaceBid(uint256 _tokenId) public payable nonReentrant(){
 
-    require(AuctionDetails[_tokenId].endAt >= block.timestamp,"Auction is finished");
+    require(AuctionDetails[_tokenId].endAt >= block.timestamp,"Auction is ended");
     require(msg.value>= AuctionDetails[_tokenId].highestBid," Bid Amount is low");
              
     if (AuctionDetails[_tokenId].highestBid < msg.value) {
@@ -141,42 +133,71 @@ function PlaceBid(uint256 _tokenId) public payable nonReentrant(){
         emit BidPlaced(_tokenId, msg.sender, msg.value);
 
     } else {
-        revert RaceAuction__BidAmountTooLow();
+        revert ("Bid Amount Too Low");
     }
 }
+/* 
+  *@param _tokenId- tokenId of the NFT
+  *@notice This function helps admin to remove NFT if no bidders for the NFT or in case of emergency.
+  *As admin call this function, the Auction stopped and the NFT transfer to Admin from the contact
+  *And highest bidder's amount also send back to his account
+  */
 
 function adminRemoveNFT(uint256 _tokenId) public onlyAdmin {
     Auction storage auctions = AuctionDetails[_tokenId];
- 
+    
     if (auctions.bidders.length == 0) {
         NFT.transferFrom(address(this),msg.sender, _tokenId);
         delete AuctionDetails[_tokenId];
         AuctionCount--;
     }
 }
+// function adminRemoveNFT(uint256 _tokenId) public onlyAdmin {
+    
+//     Auction storage auctions = AuctionDetails[_tokenId];
+
+//     //require(block.timestamp < auctions.endAt,"Auction ongoing");
+//       // require( auctions.bidders.length==0,"Bidders present to Bid") ;
+//         payable(auctions.highestBidder).transfer(auctions.highestBid);
+
+//         NFT.transferFrom(address(this), msg.sender, _tokenId);
+    
+//     delete AuctionDetails[_tokenId];
+//     AuctionCount--;
+// }
+
+
+/* 
+  *@param _tokenId- tokenId of the NFT
+  *@param _extendDuration- the time want extend
+  *@notice This function extend the duration of the Auction set only by Admin.
+  *And can extend the duration while Auction is on.
+  */
 
 
 function extendDurationOfAuction(uint256 _tokenId, uint256 _extendDuration) external onlyAdmin returns (bool) {
-   
-    if (_extendDuration == 0) {
-        revert RaceAuction__InvalidDuration();
-    }
+   require(_extendDuration != 0,"Please enter the valid duration");
+   require(AuctionDetails[_tokenId].endAt > block.timestamp,"Auction is Ended");
+
     AuctionDetails[_tokenId].duration += _extendDuration;
     AuctionDetails[_tokenId].endAt += _extendDuration;
 
     return true;
 }
 
+/* 
+  *@param _tokenId- tokenId of the NFT
+  *@param _winnerAddress- the address of the winner
+  *@notice This function is call by the Admin to transfer the Nft to the winner of the Auction
+  *function have some checks which chek the auction is live or not , and also checks the winner address is correct or not
+  */
 
 function transferNftToWinner(uint256 _tokenId, address _winnerAddress) external onlyAdmin returns(bool){
-    
-    if(block.timestamp < AuctionDetails[_tokenId].endAt){
-        revert RaceAuction__OngoingAuction();
-    }
-    if (_winnerAddress != AuctionDetails[_tokenId].highestBidder){
-        revert RaceAuction__NotWinner();
-    }
-    NFT.transferFrom(address(this),_winnerAddress,_tokenId); // transferring Nft to winner from contract
+
+    require(block.timestamp > AuctionDetails[_tokenId].endAt," Auction ongoing");
+    require(_winnerAddress == AuctionDetails[_tokenId].highestBidder,"Not winner");
+  
+    NFT.transferFrom(address(this),_winnerAddress,_tokenId);
     emit NFTTransferred(_tokenId, _winnerAddress);
 
     delete AuctionDetails[_tokenId];
@@ -186,25 +207,27 @@ function transferNftToWinner(uint256 _tokenId, address _winnerAddress) external 
 
 //NEED TO IMPROVE
 //the amount will be transfer back to the investor
-function withdrawAmount() external onlyAdmin nonReentrant() returns(bool) {
 
+/* 
+ 
+  *@notice This function is call by the Admin to withdraw the amount from the contract
+  */
+
+
+function withdrawAmount() external onlyAdmin nonReentrant() returns (bool) {
     uint256 balance = address(this).balance;
-    
-    if(balance == 0){
-        revert RaceAuction__InsufficientBalance();
-    }
+    require(balance > 0, "Insufficient Balance");
 
-    (bool success, ) = msg.sender.call{value: balance}("");
-    require(success, "Failed to transfer funds");
+    payable(msg.sender).transfer(balance);
 
     return true;
 }
-
 
 function getAuction(uint256 _tokenId) public view returns (Auction memory ){
     return AuctionDetails[_tokenId];
     }
 
-}
+  receive() external payable { }  
 
+}
 
